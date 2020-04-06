f078330d1c914936dff9a26ddaa4751f7f45c8a7
Massimo Galeotti <massi.galex@gmail.com>
2020-04-06 16:21:31 +0200

    new cocktail button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "cocktails#index"
   resources :cocktails do
     resources :doses, only: [:new, :create, :destroy]
   end
-  # get '/dose/:dose_id' to 'dose#destroy'
 end
+  # get '/dose/:dose_id' to 'dose#destroy'
+
+
+
+#
